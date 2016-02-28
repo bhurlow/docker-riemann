43e1f38af3a00beb0c852d41f25f42b2318063a9
Brian Hurlow <brian@brianhurlow.com>
2016-02-28 14:36:45 -0500

    clear out lein stuff to other branch

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject docker-riemann "1.0.0"
- :dependencies [[aleph "0.4.1-beta2"]
-                [riemann "0.2.10"]
-                [http-kit "2.1.18"]
-                [org.clojure/clojure "1.8.0"]])
